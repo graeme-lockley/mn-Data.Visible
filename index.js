3c3f1dd1ff6b565531454c79dd173223e1254234
Graeme Lockley <graemel@no9.co.za>
2017-05-03 06:32:12 +0200

    feature: initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//- The Visible interface allows values to be converted into a human-readable String representation.
+//-
+//- While not required, it is recommended that for any expression `x`, the string `x.show()` be executable JavaScript
+//- code which evaluates to the same value as the expression `x`.
+//-
+//- ```haskell
+//- interface Show a where
+//-     show :: a -> String
+//- ```
+//- This interface offers no default implementations.
+
+
+module.exports = {
+};
